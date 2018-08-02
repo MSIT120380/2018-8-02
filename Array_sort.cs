034f27c49e84ca7d3fa84dbeb9e48f10078cd8fa
exuberanteasy <exuberanteasy@gmail.com>
2018-08-02 16:58:43 +0800

    Create Array_sort.cs
    陣列排序

---
+++ Array_sort.cs
@@ -0,0 +1,6 @@
+int[] intArray = new int[5];
+Array.Sort( intArray = new int[5] { 8, 10, 2, 6, 3 });
+Array.Sort(intArray);
+// write array
+foreach (int i in intArray) Console.Write(i + " "); // output: 2 3 6 8 10
+Console.WriteLine();
